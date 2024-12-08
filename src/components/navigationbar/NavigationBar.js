8c47a602778918d1f2a10d411239816273bd2735
pinocchioph1129 <kjavo2@illinois.edu>
2024-12-08 17:14:39 -0600

    Updated for mobile.

--- src/components/navigationbar/NavigationBar.js
+++ src/components/navigationbar/NavigationBar.js
@@ -1,33 +1,61 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import { Icon } from "@iconify/react";
+import { useState } from "react";
 import "./NavigationBar.css";
+
 export default function NavigationBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
   return (
     <nav className="nav-wrapper">
       <div className="nav-container">
         <div className="logo-container">
           <Icon icon="unjs:unstorage" width="25" height="25" />{" "}
-          <Link to="/" className="site-title">
+          <Link to="/" className="site-title" onClick={closeMenu}>
             Kervi Jay Javonillo
           </Link>
         </div>
-        <ul>
-          <CustomLink to="/Notes">Notes</CustomLink>
-          <CustomLink to="/Articles">Articles</CustomLink>
-          <CustomLink to="/Projects">Projects</CustomLink>
-          <CustomLink to="/About">About Me</CustomLink>
+        <button className="menu-toggle" onClick={toggleMenu}>
+          ☰
+        </button>
+        <ul className={isMenuOpen ? "open" : ""}>
+          <CustomLink to="/Notes" closeMenu={closeMenu}>
+            Notes
+          </CustomLink>
+          <CustomLink to="/Articles" closeMenu={closeMenu}>
+            Articles
+          </CustomLink>
+          <CustomLink to="/Projects" closeMenu={closeMenu}>
+            Projects
+          </CustomLink>
+          <CustomLink to="/About" closeMenu={closeMenu}>
+            About Me
+          </CustomLink>
         </ul>
       </div>
     </nav>
   );
 }
 
-function CustomLink({ to, children, ...props }) {
+function CustomLink({ to, children, closeMenu, ...props }) {
   const resolvedPath = useResolvedPath(to);
   const isActive = useMatch({ path: resolvedPath.pathname, end: true });
   return (
     <li className={isActive ? "active" : ""}>
-      <Link to={to} {...props}>
+      <Link
+        to={to}
+        {...props}
+        onClick={() => {
+          if (closeMenu) closeMenu();
+        }}
+      >
         {children}
       </Link>
     </li>
