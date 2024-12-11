5aec63dd84b96ecb28e5e1f452adc787dec8ab4e
pinocchioph1129 <kjavo2@illinois.edu>
2024-12-10 18:32:51 -0600

    Updated. Dec 9th, 2024.

--- src/components/notes/2024/firstnote.js
+++ src/components/notes/2024/firstnote.js
@@ -7,25 +7,23 @@ import { useEffect } from "react";
 export default function Page() {
   useEffect(() => {
     const COMMENTS_ID = "utterances-comments-script";
-
     if (!document.getElementById(COMMENTS_ID)) {
-      console.log("Utterances script is loading...");
       const script = document.createElement("script");
       script.src = "https://utteranc.es/client.js";
       script.async = true;
       script.setAttribute("repo", "pinocchioph1129/personal-web");
-      script.setAttribute("issue-term", "pathname");
+
+      if (process.env.NODE_ENV === "production") {
+        script.setAttribute("issue-term", "pathname");
+      } else {
+        console.log("Utterances not loaded in development.");
+        return;
+      }
+
       script.setAttribute("theme", "github-light");
       script.setAttribute("crossorigin", "anonymous");
       script.id = COMMENTS_ID;
-
-      const commentContainer = document.getElementById("utterances-comments");
-      if (commentContainer) {
-        commentContainer.appendChild(script);
-        console.log("Utterances script appended successfully.");
-      } else {
-        console.error("Error: Utterances container not found.");
-      }
+      document.getElementById("utterances-comments").appendChild(script);
     }
   }, []);
 
